c5877f0713385dad959d53a479cc5b90d1045d7d
Joh E <johannes.ehala@ttu.ee>
2022-04-11 14:34:09 +0300

    Serial parser now prints heartbeat message.

--- serial_parser/pars_serial_direct.cpp
+++ serial_parser/pars_serial_direct.cpp
@@ -3,7 +3,7 @@
  *        special token. Then starts logging all subsequent non-token bytes. 
  * 
  * @usage 
- *        jpnevulator -read -t /dev/ttyUSB0 | ./pars_serial_direct
+ *        jpnevulator -read -t /dev/ttyUSB0 | ./pars_serial_direct results-filename
  *        baud rate 115200
  *
  * @note this little-endian-big-endian business makes the code complicated to understand
@@ -37,7 +37,7 @@ void sigint_handler(int sig)
 
 int main(int argc, char **argv) 
 {
-	int i, bytes;
+	int i, bytes, total_bytes;
 	bool firsttime = true;
 	char filename[NUM_FILE_NAME_CHARACTERS];
 
@@ -77,7 +77,9 @@ int main(int argc, char **argv)
 			}
 			else ;
 			bytes++;
+			total_bytes++;
 			write_to_log();
+			if(total_bytes%100000 == 0)printf("Bytes received so far %u\n", total_bytes);
 		}
 	}
 	return 0;
